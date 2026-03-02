3d7705e449cfdcbe006b831ee01504a6ba16cdc9
gitsujal01 <sujallokhande23@gmail.com>
2026-03-03 00:14:07 +0530

    Fix env vars for mongo and frontend url

--- src/main/java/com/substring/chat/config/AppConstants.java
+++ src/main/java/com/substring/chat/config/AppConstants.java
@@ -1,6 +1,11 @@
 package com.substring.chat.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AppConstants {
 
-	 public static final String FRONT_END_BASE_URL = System.getenv("FRONT_END_BASE_URL");
+    @Value("${frontend.base.url}")
+    public String FRONT_END_BASE_URL;
 }
