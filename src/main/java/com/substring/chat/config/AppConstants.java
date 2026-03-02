a0af6f755af14e670e1cc07797e1b7294646945a
gitsujal01 <sujallokhande23@gmail.com>
2026-03-03 00:43:37 +0530

    changes in application.properties

--- src/main/java/com/substring/chat/config/AppConstants.java
+++ src/main/java/com/substring/chat/config/AppConstants.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppConstants {
 
+    public static String FRONT_END_BASE_URL;
+
     @Value("${frontend.base.url}")
-    public String FRONT_END_BASE_URL;
+    public void setFrontEndBaseUrl(String url) {
+        FRONT_END_BASE_URL = url;
+    }
 }
